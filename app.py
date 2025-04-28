84e6bf610e3c92dac7ba689f55138f678b220c52
Rajveer Tamrakar <rajishere12345@gmail.com>
2025-04-28 20:20:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,56 @@
+import cv2
+from keras.models import model_from_json # type: ignore
+import numpy as np
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# Load model and cascade
+json_file = open("detector.json", "r")
+model_json = json_file.read()
+json_file.close()
+model = model_from_json(model_json)
+model.load_weights("emotiondetector.h5")
+haar_file = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+face_cascade = cv2.CascadeClassifier(haar_file)
+
+labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+
+def extract_features(image):
+    feature = np.array(image)
+    feature = feature.reshape(1, 48, 48, 1)
+    return feature / 255.0
+
+def generate_frames():
+    webcam = cv2.VideoCapture(0)
+    while True:
+        success, frame = webcam.read()
+        if not success:
+            break
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (p, q, r, s) in faces:
+            image = gray[q:q+s, p:p+r]
+            cv2.rectangle(frame, (p, q), (p+r, q+s), (255, 0, 0), 2)
+            image = cv2.resize(image, (48, 48))
+            img = extract_features(image)
+            pred = model.predict(img)
+            prediction_label = labels[pred.argmax()]
+            cv2.putText(frame, prediction_label, (p-10, q-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255))
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
